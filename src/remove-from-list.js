2c4bb923b3588fcf1bec1b99a2f0b91d1a2f7953
Oksana <5oks5@mail.ru>
2023-04-13 17:28:40 +0400

    feat: add remove-from-list solution

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -15,26 +15,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be [1, 2, 4, 5]
  *
  * Singly - linked lists are already defined using interface
- * class ListNode {
- *   constructor(x) {
- *     this.value = x;
- *     this.next = null;
- *   }
- * }
+
  */
-function removeKFromList(l, k) {
-  let arr = l;
-  while ( l.value == k) {
-    arr = l.next;
-  }
-  while ( arr.next !== null) {
-    if(arr.next.value === k) {
-      arr.next = arr.next.next;
-    } else {
-      arr = arr.next;
+
+class ListNode {
+     constructor(x) {
+      this.value = x;
+      this.next = null;
     }
+   }
+
+function removeKFromList(list, k) {
+  if(list === null) return null;
+let newNode = new ListNode();
+newNode.next = list;
+
+let currElement = newNode;
+
+while(currElement.next) {
+  if(currElement.next.value === k) {
+    currElement.next = currElement.next.next;
+  } else {
+    currElement = currElement.next;
   }
-return l;
+}
+
+return newNode.next
 }
 
 module.exports = {
